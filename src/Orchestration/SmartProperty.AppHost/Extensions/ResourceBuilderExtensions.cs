bfc57e4ff343481fb17a25b24052f37acf9fefa4
Deyvit Cholakov <87949806+deivit98@users.noreply.github.com>
2026-02-21 21:01:04 +0200

    Added DB (#3)
    * add postgres
    
    * remove sample classes
    
    * added db

---
+++ src/Orchestration/SmartProperty.AppHost/Extensions/ResourceBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace SmartProperty.AppHost.Extensions
+{
+    public static class ResourceBuilderExtensions
+    {
+        extension(IResourceBuilder<ProjectResource> builder)
+        {
+            public IResourceBuilder<ProjectResource> WaitWithReference(IResourceBuilder<OllamaResource> ollama)
+            {
+                return builder
+                    .WithReference(ollama)
+                    .WaitFor(ollama);
+            }
+
+            public IResourceBuilder<ProjectResource> WaitWithReference(IResourceBuilder<OllamaModelResource> ollamaModel)
+            {
+                return builder
+                    .WithReference(ollamaModel)
+                    .WaitFor(ollamaModel);
+            }
+
+            public IResourceBuilder<ProjectResource> WaitWithReference(IResourceBuilder<QdrantServerResource> qdrant)
+            {
+                return builder
+                    .WithReference(qdrant)
+                    .WaitFor(qdrant);
+            }
+
+            public IResourceBuilder<ProjectResource> WaitWithReference(IResourceBuilder<PostgresServerResource> postgres, IResourceBuilder<PostgresDatabaseResource> db)
+            {
+                return builder
+                     .WithReference(postgres)
+                     .WithReference(db)
+                     .WaitFor(postgres)
+                     .WaitFor(db);
+
+            }
+        }
+    }
+}
